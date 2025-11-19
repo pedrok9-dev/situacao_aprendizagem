57fed1725ab103ac7002a295545cca37b3db0e80
PEDRO AUGUSTO BECHTOLD THEIS <pedro_theis@estudante.sesisenai.org.br>
2025-11-19 08:33:36 -0300

    V1.6

--- java.js
+++ java.js
@@ -12,7 +12,7 @@ let res = document.getElementById('res')
 let arr = [7, 14, 2, 19, 11, 4, 16, 1, 9, 6, 18, 3, 12, 20, 5, 17, 8, 13, 10, 15]
 let val = 0
 
-      res.innerHTML = ` `
+res.innerHTML = ` `
 
 function ordenarCres(){
     for( let j = 0; j < arr.length - 1; j++){
@@ -42,4 +42,57 @@ function ordenarDesc(){
 
     res.innerHTML += `ordem decrescente: ${arr} <br>`
 
+}
+
+let res2 = document.getElementById('res2')
+let arrAleat = []
+let val2 = 0
+
+res2.innerHTML = ` `
+
+function numAleat(min,max){
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function ordenarAleat(){
+
+    for(let i = 0; i < 10; i++){
+        arrAleat[i] = numAleat(1,20)
+    }
+
+    let arrOriginal = [...arrAleat]
+
+    for( let j = 0; j < arrAleat.length - 1; j++){
+        for(let i = 0; i < arrAleat.length - j - 1; i++){
+            if(arrAleat[i] > arrAleat[i+1]){
+                val2 = arrAleat[i]
+                arrAleat[i] = arrAleat[i+1]
+                arrAleat[i+1] = val2
+            }
+        }
+    }
+
+    res2.innerHTML += `Ordem errada: ${arrOriginal}<br>`
+    res2.innerHTML += `Ordem certa: ${arrAleat}`
+}
+
+let res3 = document.getElementById('res3')
+
+let soma = 0
+let num = []
+
+res3.innerHTML = ` `
+
+function numAleat220(min,max){
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function somar220(){
+    for(let i = 0; i < 10; i++){
+        num[i] = numAleat220(1,220)
+        soma += num[i]
+    }
+
+    res3.innerHTML += `Os números sorteados foram: ${num}<br>`
+    res3.innerHTML += `A soma total foi: ${soma}`
 }
