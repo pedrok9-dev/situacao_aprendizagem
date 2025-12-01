27e3c0f72d71f813600b2e53c7e287514a0a93f4
pedro theis <pedro_theis@estudante.sesisenai.org.br>
2025-12-01 20:02:11 -0300

    versao_final

--- java.js
+++ java.js
@@ -216,3 +216,48 @@ function buscar() {
         res7.innerHTML = "Valor não encontrado.";
     }
 }
+
+let res8 = document.getElementById('res8')
+
+function tensao(){
+    let corren = Number(document.getElementById('corren').value);
+    let resis = Number(document.getElementById('resis').value);
+    let tensao = 0
+    
+    res8.innerHTML = ` `
+
+    tensao = corren * resis
+
+    res8.innerHTML += `A tensão é de ${tensao}V`
+}
+
+let res9 = document.getElementById('res9')
+
+function resis(){
+    let corren2 = Number(document.getElementById('corren2').value);
+    let tens2 = Number(document.getElementById('tens2').value);
+    let resi = tens2 / corren2    
+    res9.innerHTML = ` `
+
+    res9.innerHTML += `A Resitência é de ${resi.toFixed(2)} Ohms` 
+}
+
+let res11 = document.getElementById('res11')
+
+function coren(){
+    let resis3 = Number(document.getElementById('resis3').value);
+    let tens3 = Number(document.getElementById('tens3').value);
+    let coren = tens3 / resis3    
+    res11.innerHTML = ` `
+
+    res11.innerHTML += `A Corrente é de ${coren.toFixed(2)}A`  
+}
+
+function poten(){
+    let coren4 = Number(document.getElementById('coren4').value);
+    let tens4 = Number(document.getElementById('tens4').value);
+    let poten = tens4 / coren4    
+    res11.innerHTML = ` `
+
+    res11.innerHTML += `A Potência é de ${poten.toFixed(2)}W`  
+}
